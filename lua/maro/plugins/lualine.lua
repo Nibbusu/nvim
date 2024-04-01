1acfeb8da65473c837e0def862bc3359e0b25b5b
Maro <maro@kopera.fyi>
2024-04-01 18:16:24 +0200

    added bubble separators for lualine

--- lua/maro/plugins/lualine.lua
+++ lua/maro/plugins/lualine.lua
@@ -7,7 +7,7 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
- -- component_separators = { left = '', right = ''},
+    --component_separators = { left = '', right = ''},
     component_separators = '',
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
@@ -16,7 +16,7 @@ require('lualine').setup {
     },
     ignore_focus = {},
     always_divide_middle = true,
-    globalstatus = false,
+    globalstatus = true,
     refresh = {
       statusline = 1000,
       tabline = 1000,
@@ -24,13 +24,13 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = { { 'mode', separator = { left = '' } } },
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
  -- lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_x = {'filetype'},
     lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_z = {{ 'location', separator = { right = '' }}}
   },
   inactive_sections = {
     lualine_a = {},
